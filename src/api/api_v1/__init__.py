2cd96cb68e3eb6dbcc2d9e215280f2adace6fdc1
Jara komp <jara@komp.com>
2025-12-10 14:27:06 +0200

    fix router

--- src/api/api_v1/__init__.py
+++ src/api/api_v1/__init__.py
@@ -3,14 +3,12 @@ from fastapi import APIRouter
 from core.config import settings
 from .users import router as users_router
 from .fipers import router as fipers_router
-from .decontamination import router as decontamination_router
-from .dec_preset import router as dec_preset_router
+from .jupers import router as jupers_router
 
 ROUTERS: list[tuple[APIRouter, str]] = [
     (users_router, settings.api.v1.users),
     (fipers_router, settings.api.v1.fipers),
-    (decontamination_router, settings.api.v1.decontaminations),
-    (dec_preset_router, settings.api.v1.dec_presets),
+    (jupers_router, settings.api.v1.jupers),
 ]
 
 router = APIRouter()
