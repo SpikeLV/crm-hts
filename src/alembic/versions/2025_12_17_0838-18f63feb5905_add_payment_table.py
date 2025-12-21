59471a941356900b0dfbaad9d530076c95cb15f3
Jara komp <jara@komp.com>
2025-12-21 22:52:08 +0200

    fix in schema for payment.description null=true

--- src/alembic/versions/2025_12_17_0838-18f63feb5905_add_payment_table.py
+++ src/alembic/versions/2025_12_17_0838-18f63feb5905_add_payment_table.py
@@ -9,6 +9,7 @@ Create Date: 2025-12-17 08:38:50.639078
 from typing import Sequence, Union
 
 from alembic import op
+from pydantic.deprecated.tools import T
 import sqlalchemy as sa
 
 
@@ -22,11 +23,11 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         "payments",
+        sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("amount", sa.Numeric(precision=10, scale=2), nullable=False),
         sa.Column("date", sa.Date(), nullable=False),
-        sa.Column("description", sa.String(length=255), nullable=False),
+        sa.Column("description", sa.String(length=255), nullable=True),
         sa.Column("invoice_id", sa.Integer(), nullable=False),
-        sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("created_at", sa.DateTime(), nullable=False),
         sa.Column("updated_at", sa.DateTime(), nullable=True),
         sa.Column("deleted_at", sa.DateTime(), nullable=True),
